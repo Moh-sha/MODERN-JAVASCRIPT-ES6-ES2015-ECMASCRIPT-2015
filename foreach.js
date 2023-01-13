8c6a16a25dc76a320a43b8d29c5f01f323c05d10
Shafin_ahmed <Ahmedshafin112233@gmail.com>
2023-01-14 04:48:39 +0600

    part 3

---
+++ foreach.js
@@ -0,0 +1,9 @@
+const numbers = [2, 3, 4, 5, 6];
+const squre = [];
+numbers.forEach(function (x) {
+  console.log(x);
+  const result = x * x;
+  squre.push(result);
+  
+});
+console.log(squre);
---
+++ map.js
@@ -0,0 +1,5 @@
+const numbers = [2, 3, 4, 5, 6];
+const squre = numbers.map(function (x) {
+  return x * x;
+});
+console.log(squre);
